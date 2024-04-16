8e77f889f91a97605145ee055303e0911a727aad
pnquynhdev <pnquynh.dev@gmail.com>
2024-04-16 09:38:39 +0700

    done backend without jwt

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 // import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
@@ -14,6 +15,7 @@ app.use(express.json());
 // app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, (req, res) => {
   connectToMongoDB();

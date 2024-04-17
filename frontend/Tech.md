4c36081d423349474f15dcd7c592e8459d5b7cda
pnquynhdev <pnquynh.dev@gmail.com>
2024-04-17 11:15:00 +0700

    UI design

--- frontend/Tech.md
+++ frontend/Tech.md
@@ -1,2 +1,7 @@
 - [Install Tailwind CSS with Vite](https://tailwindcss.com/docs/guides/vite)
 - [Install daisyUI as a Tailwind CSS plugin](https://daisyui.com/docs/install/)
+- Extensions:
+    - Tailwind CSS IntelliSense, 
+    - ES7 React/Redux/GraphQL/React-Native snippets 
+
+- [Tailwindcss Glassmorphism Generator](https://tailwindcss-glassmorphism.vercel.app/)
---
+++ frontend/src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import MessageContainer from "../../components/messages/MessageContainer";
+import Sidebar from "../../components/sidebar/Sidebar";
+
+const Home = () => {
+  return (
+    <div className="flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-0  bg-clip-padding backdrop-filter backdrop-blur-small bg-opacity-50 ">
+      <Sidebar />
+      <MessageContainer />
+    </div>
+  );
+};
+export default Home;
